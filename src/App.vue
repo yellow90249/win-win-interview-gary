9773f8a69703f4379b876eb82c49981d67075123
gary <yellow90249@gmail.com>
2024-03-15 14:52:32 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Post v-for="post in postList" :post :key="post.id"></Post>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Post from '@/components/Post.vue';
+import { ref } from 'vue';
+const postList = ref([
+  {
+    id: 1,
+    title: 'Post Title',
+    name: 'User',
+    date: '2021.09.21',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+
+    tag: ['英文', '故事', '精選優質'],
+    misc: { like: 100, sub: 100, com: 100, watch: 100 },
+  },
+  {
+    id: 1,
+    title: 'Post Title',
+    name: 'User',
+    date: '2021.09.21',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+
+    tag: ['日文', '文章'],
+    misc: { like: 100, sub: 100, com: 100, watch: 100 },
+  },
+]);
+</script>
+
+<style>
+body {
+  background-color: #eee;
+  margin: 0;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="post-container">
+    <h1 class="post-title mb">{{ post.title }}</h1>
+    <div class="post-author mb">
+      <div class="img"></div>
+      <div class="name">{{ post.name }}</div>
+      <div class="date">{{ post.date }}</div>
+    </div>
+    <div class="post-content mb">{{ post.content }}</div>
+    <div class="post-img mb">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+    <div class="post-bottom">
+      <div class="tags">
+        <div class="tag" v-for="tag in post.tag">{{ tag }}</div>
+      </div>
+      <div class="misc">
+        <div class="item">點贊:{{ post.misc.like }} ·</div>
+        <div class="item">收藏:{{ post.misc.sub }} ·</div>
+        <div class="item">頻論:{{ post.misc.com }} ·</div>
+        <div class="item">觀看:{{ post.misc.watch }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  post: {
+    id: number;
+    title: string;
+    name: string;
+    date: string;
+    content: string;
+    tag: string[];
+    misc: {
+      like: number;
+      sub: number;
+      com: number;
+      watch: number;
+    };
+  };
+}>();
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  color: #333;
+}
+
+.mb {
+  margin-bottom: 8px;
+}
+
+.post-container {
+  font-family: sans-serif;
+  background-color: #fff;
+  padding: 10px 20px;
+  margin-bottom: 10px;
+}
+
+.post-title {
+  font-size: 20px;
+}
+
+.post-author {
+  display: flex;
+}
+
+.post-author .img {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #ccc;
+  margin-right: 4px;
+}
+
+.post-author .name {
+  margin-right: 8px;
+}
+
+.post-img {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+
+.post-img .item {
+  height: 80px;
+  background-color: #ccc;
+  border-radius: 8px;
+}
+
+.post-bottom {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px 10px;
+}
+
+.tags {
+  display: flex;
+  gap: 4px;
+}
+
+.tags .tag {
+  font-size: 12px;
+  background-color: #eee;
+  padding: 4px;
+  border-radius: 8px;
+}
+
+.misc {
+  display: flex;
+}
+
+.misc .item {
+  font-size: 12px;
+  padding: 4px;
+}
+</style>
